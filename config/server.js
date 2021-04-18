f8b56a19f6970dad0ddc6eb0ffc9f0d2d4ea7915
lulaluano <luanferreira2136@gmail.com>
2021-04-18 18:19:29 +0000

    ops agora sim

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://brcadv.com/api',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'ff1be59491d100a44b775ac29ef91719'),
